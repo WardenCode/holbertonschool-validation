919d8ba60a6af4b71416387c2f9b920f830bb631
Diego Linares <diegojeanluck@hotmail.com>
2023-01-08 20:18:34 -0500

    Fix: don't run golangci-lint

--- Docker/module4_task0/setup.sh
+++ Docker/module4_task0/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 apt-get update && apt-get install -y hugo make shellcheck nodejs
 npm install -g markdownlint-cli
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.50.1
-
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)/bin" v1.50.1
