7bbd76e350ed716176635cf8a1dcd400e25deadc
Diego Linares <diegojeanluck@hotmail.com>
2023-01-08 20:22:36 -0500

    New line added

--- Docker/module4_task0/setup.sh
+++ Docker/module4_task0/setup.sh
@@ -2,3 +2,4 @@
 apt-get update && apt-get install -y hugo make shellcheck nodejs
 npm install -g markdownlint-cli
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)/bin" v1.50.1
+
