c4ec6713bad3bb80200dee0e88b5d1942d861b53
Diego Linares <diegojeanluck@hotmail.com>
2023-01-08 19:06:19 -0500

    Building Inside a Docker Container

---
+++ Docker/module4_task0/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get update && apt-get install -y hugo make shellcheck nodejs
+npm install -g markdownlint-cli
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.50.1
+export PATH="$(PATH):/$(go env GOPATH)/bin"
